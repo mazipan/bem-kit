d4cd177284f8cbf314e3fba1b8e6c7a1b94181cc
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-03-29 10:04:44 +0700

    Add browsersync config

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,90 +1,100 @@
 var gulp = require('gulp');
+
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 var clean = require('gulp-clean');
+var cleanCSS = require('gulp-clean-css');
+var header = require('gulp-header');
+var pump = require('pump');
+var rename = require("gulp-rename");
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var header = require('gulp-header');
-var rename = require("gulp-rename");
-var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
-var pump = require('pump');
 
 var input = ['./src/**/*.scss'];
 var output = './dist';
 
 var pkg = require('./package.json');
 var banner = ['/*!',
-              ' * <%= pkg.name %> v<%= pkg.version %>',
-              ' * @author : Irfan Maulana',
-              ' * @repo   : https://github.com/mazipan/bem-kit',
-              ' */',
-              ''
-              ].join('\n');
+    ' * <%= pkg.name %> v<%= pkg.version %>',
+    ' * @author : Irfan Maulana',
+    ' * https://github.com/mazipan/bem-kit',
+    ' */',
+    ''
+].join('\n');
 
+var autoprefixerOptions = {
+    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
 
-gulp.task('clean', function () {
-    return gulp.src([output], {read: false})
-        .pipe(clean({force: true}));
+var sassOptions = {
+    outputStyle: 'compact'
+};
+
+gulp.task('clean', function() {
+    return gulp.src([output], { read: false })
+        .pipe(clean({ force: true }));
 });
 
-gulp.task('sass', ['clean'], function () {
+gulp.task('serve', ['simple-sass'], function() {
 
-    var autoprefixerOptions = {
-        browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
-    };
+    browserSync.init({
+        port: 3000,
+        server: "./demo"
+    });
 
-    var sassOptions = {
-        outputStyle: 'compact'
-    };
+    gulp.watch(input, ['simple-sass']);
+    gulp.watch("./demo/*.html").on('change', browserSync.reload);
+});
 
+gulp.task('simple-sass', function() {
     return gulp
         .src(input)
-        .pipe(sourcemaps.init())
-        .pipe(sass(sassOptions))
-        .pipe(sourcemaps.write())
+        .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(header(banner, { pkg : pkg } ))
+        .pipe(gulp.dest('./demo/dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('sass', ['clean'], function() {
+    return gulp
+        .src(input)
+        .pipe(sass(sassOptions))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest(output));
 });
 
-gulp.task('watch', ['sass'], function() {
-    return gulp
-        .watch(input, ['sass'])
-        .on('change', function(event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
-});
+gulp.task('uglify-js', ['clean'], function(cb) {
 
-gulp.task('uglify-js', ['clean'], function (cb) {
+    var uglifyOptions = {
+        mangle: true,
+        preserveComments: 'license'
+    };
 
-  var uglifyOptions = {
-    mangle: true,
-    preserveComments: 'license'
-  };
- 
-  pump([
-      gulp.src('./src/*.js'),
-      uglify(),
-      gulp.dest(output)
+    pump([
+        gulp.src('./src/*.js'),
+        uglify(),
+        gulp.dest(output)
     ], cb);
 
 });
 
-gulp.task('rename', ['sass', 'uglify-js'], function () {
-     return gulp.src(["./dist/*.css", "./dist/*.js"])
-          .pipe(rename(function (path) {
+gulp.task('rename', ['sass', 'uglify-js'], function() {
+    return gulp.src(["./dist/*.css", "./dist/*.js"])
+        .pipe(rename(function(path) {
             path.basename += ".min";
-          }))
-          .pipe(gulp.dest(output));
+        }))
+        .pipe(gulp.dest(output));
 });
 
 gulp.task('minify-css', ['rename'], function() {
     return gulp.src('./dist/*.min.css')
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest(output));
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest(output));
 });
 
 
 gulp.task('build:prod', ['clean', 'sass', 'uglify-js', 'rename', 'minify-css']);
-gulp.task('build:dev',  ['clean', 'sass', 'watch']);
-gulp.task('default',    ['build:dev']);
+gulp.task('build:dev', ['clean', 'sass', 'serve']);
+gulp.task('default', ['build:dev']);
